6327b990e7b645f9c439f549dba063559ab3dc28
Ricardo <richavesca@est.utn.ac.cr>
2025-11-09 18:32:09 -0600

    Se creo la clase Metodos de Juego aplicando Strategy
    Se creo la interfaz y las diferentes clases de juego normal, carton lleno, cuatro esquinas, etc.

--- src/Modelo/Strategy/CartonLleno.java
+++ src/Modelo/Strategy/CartonLleno.java
@@ -4,10 +4,31 @@
  */
 package Modelo.Strategy;
 
+import Modelo.Cartones.CartonBingo;
+
 /**
  *
  * @author Ricardo Chaves
  */
-public class CartonLleno {
+public class CartonLleno implements ReglaVictoria{
+
+    @Override
+    public boolean esGanador(CartonBingo carton) {
+        boolean[][] marcado = carton.getMarcados();
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i == 2 && j == 2) continue;
+                if(marcado[i][j]==false){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String getNombreRegla() {
+        return "Gano con carton lleno";
+    }
     
 }
--- src/Modelo/Strategy/VictoriaNormal.java
+++ src/Modelo/Strategy/VictoriaNormal.java
@@ -4,10 +4,60 @@
  */
 package Modelo.Strategy;
 
+import Modelo.Cartones.CartonBingo;
+
 /**
  *
  * @author Ricardo Chaves
  */
-public class VictoriaNormal {
+public class VictoriaNormal implements ReglaVictoria {
+
+    @Override
+    public boolean esGanador(CartonBingo carton) {
+        //Forma horizontal
+        boolean[][] marcados = carton.getMarcados();
+        
+        for (int i = 0; i < 5; i++) {
+            boolean filacompleta = true;
+            for (int j = 0; j < 5; j++) {
+                if(!marcados[i][j]) filacompleta = false;
+            }
+            if(filacompleta) return true;
+        }
+        
+        //Forma Vertical
+        for (int j = 0; j < 5; j++) {
+            boolean columnacompleta = true;
+            for (int i = 0; i < 5; i++) {
+                if(!marcados[i][j]) columnacompleta = false;
+            }
+            if(columnacompleta) return true;
+        }
+        
+        //Formato Cuatro Esquinas
+        boolean esquinaSuperiorIzq = marcados[0][0];
+        boolean esquinaSuperiorDer = marcados[0][4];
+        boolean esquinaInferiorIzq = marcados[4][0];
+        boolean esquinaInferiorDer = marcados[4][4];
+        
+        // Si las cuatro esquinas están marcadas, gana también
+        if (esquinaSuperiorIzq && esquinaSuperiorDer && esquinaInferiorIzq && esquinaInferiorDer) {
+        return true;
+        }
+        
+        //Forma Diagonal
+        boolean diagonalPrincipal = true, diagonalSecundaria = true;
+        for (int i = 0; i < 5; i++) {
+            if (!marcados[i][i]) diagonalPrincipal = false;
+            if (!marcados[i][4 - i]) diagonalSecundaria = false;
+        }
+        return diagonalPrincipal || diagonalSecundaria;
+ 
+    }
+
+    @Override
+    public String getNombreRegla() {
+        return "Has ganado.";
+    }
     
 }
