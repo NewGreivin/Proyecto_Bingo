a22ec288a87ce39cdde31b9cce2fee388467ae3b
Ricardo <richavesca@est.utn.ac.cr>
2025-11-09 20:58:55 -0600

    Se creo la clase repositorio
    Se creo la clase repositorio de carton con su metodo agregar, eliminar y listar aplicando singleton.

--- src/Modelo/Repositorio/RepositorioCarton.java
+++ src/Modelo/Repositorio/RepositorioCarton.java
@@ -4,10 +4,43 @@
  */
 package Modelo.Repositorio;
 
+import Modelo.Cartones.CartonBingo;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
 /**
  *
  * @author Ricardo Chaves
  */
 public class RepositorioCarton {
+    private static RepositorioCarton instancia; 
+    private HashMap<String, CartonBingo> cartones;
+
+    public static RepositorioCarton getInstancia() {
+        if (instancia == null) {
+            instancia = new RepositorioCarton();
+        }
+        return instancia;
+    }
     
-}
+    private RepositorioCarton() {
+        cartones = new HashMap<>();
+    }
+    
+    public void agregarCarton(CartonBingo carton){
+        cartones.put(carton.getId(), carton);
+    }
+    
+    public void eliminarCarton(String id){
+        if (cartones.containsKey(id)) {
+            cartones.remove(id);
+        }
+    }
+    
+    public Collection<CartonBingo> obtenerCartones() {
+        return cartones.values();
+    }
+
+} 
