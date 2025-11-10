ff2ca6c1c795fb72f49987bb7661170e915354fb
Ricardo <richavesca@est.utn.ac.cr>
2025-11-09 22:26:59 -0600

    Se creo la clase servicio
    Se creo la clase con el metodo agregar y elimar carton

--- src/Modelo/Servicios/ServicioCarton.java
+++ src/Modelo/Servicios/ServicioCarton.java
@@ -4,10 +4,34 @@
  */
 package Modelo.Servicios;
 
+import Modelo.Cartones.CartonBingo;
+import Modelo.Fabrica.FabricaCarton;
+import Modelo.Repositorio.RepositorioCarton;
+
 /**
  *
  * @author Ricardo Chaves
  */
 public class ServicioCarton {
+    private final RepositorioCarton repo = RepositorioCarton.getInstancia();
     
+    public CartonBingo crearCarton(boolean automatico, int valores[][]){
+        
+        String id = "C" + (repo.obtenerCartones().size() + 1);
+        CartonBingo carton;
+        
+        if (automatico)
+            carton = FabricaCarton.crearCartonAutomatico(id);
+        else
+            carton = FabricaCarton.crearCartonManual(id, valores);
+
+        if (carton != null && Utilidades.Validador.validarFormatoCarton(carton.getNumero()))
+            repo.agregarCarton(carton);
+
+        return carton;
+    }
+    
+    public void eliminarCarton(String id){
+        repo.eliminarCarton(id);
+    }
 }
