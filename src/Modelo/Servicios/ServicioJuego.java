9a15b4252133971c58582f9ce2ec81440d00e17b
New Greivin <grarguedasgu@est.utn.ac.cr>
2025-11-12 09:14:15 -0600

    Creación de clase ServicioBingo
    Se encarga de orquestar el estado del bingo

--- src/Modelo/Servicios/ServicioJuego.java
+++ src/Modelo/Servicios/ServicioJuego.java
@@ -1,13 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**
+ * @author Greivin
  */
 package Modelo.Servicios;
 
-/**
- *
- * @author Ricardo Chaves
- */
+import Modelo.Cartones.CartonBingo;
+import Modelo.Estados.EstadoJuego;
+import Modelo.Repositorio.RepositorioEstadoJuego;
+import Modelo.Strategy.ReglaVictoria;
+
 public class ServicioJuego {
-    
+    private final RepositorioEstadoJuego repo;
+
+    public ServicioJuego() {
+        this.repo = new RepositorioEstadoJuego();
+    }
+
+    public void aplicarNumero(int numero) {
+        EstadoJuego estado = repo.obtenerEstado();
+
+        estado.getTablero().setNumerosMarcados(numero); //Marcar en cada tablero
+
+        for (CartonBingo c : estado.getListaCartones()) { //Marcar en el cada carton
+            c.marcarNumero(numero);
+        }
+    }
+
+    public CartonBingo verificarGanador() {
+        EstadoJuego estado = repo.obtenerEstado();
+        ReglaVictoria regla = estado.getEstrategiaActual();
+
+        for (CartonBingo c : estado.getListaCartones()) {
+            if (regla.esGanador(c)) {
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public void reiniciarJuego() {
+        repo.obtenerEstado().reiniciarJuego();
+    }
 }
